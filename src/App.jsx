e11248556ebe3f03505e17c5ceacd6ca0f1bff15
Joel Martinez <jm8587700@gmail.com>
2023-07-23 18:48:17 -0300

    web1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,152 @@
+import "./app.css";
+import "animate.css";
+import fondoSpiderMan from "./img/fondo-spider-man-max.png";
+import spiderManPng from "./img/spider-man-max.png";
+import { useEffect, useState, useRef } from "react";
+import { history } from "./info";
+import araña from "./img/araña-spider-man.png";
+
+
+export const App = () => {
+  const [movimiento, setMovimiento] = useState(0);
+  const [obtenerID, setObtenerID] = useState(0);
+  const [showAnimation, setShowAnimation] = useState(false);
+  const [descripcionID, setdescripcionID] = useState([]);
+  const [wordObtenidas, setWordObtenidas] = useState([]);
+
+  useEffect(() => {
+    let evScroll = () => {
+      let scroll = window.scrollY;
+      setMovimiento(scroll);
+    };
+    window.addEventListener("scroll", evScroll);
+    return () => {
+      window.removeEventListener("scroll", evScroll);
+    };
+  }, [movimiento]);
+
+  useEffect(() => {
+    let descripcion = history.find((img) => img.id === obtenerID);
+    if (descripcion) {
+      setdescripcionID(descripcion.descripcion.split(""));
+    }
+  }, [obtenerID]);
+
+  useEffect(() => {
+    let word = 0;
+    let interval = setInterval(() => {
+      if (word < descripcionID.length || word === undefined) {
+        setWordObtenidas((prevWord) => ([...prevWord, descripcionID[word]]));
+        word++;
+      }
+    }, 60);
+
+    return () => clearInterval(interval);
+  }, [descripcionID]);
+
+
+  useEffect(() => {
+    setWordObtenidas([])
+
+  }, [descripcionID]);
+
+
+   const secInfoConteinerText =useRef()
+   useEffect(()=>{
+     let elementReferent =  secInfoConteinerText.current.offsetTop;
+    
+     if(movimiento >= elementReferent - 20){
+      setShowAnimation(true)
+     }else setShowAnimation(false)
+   },[showAnimation]
+   )
+
+
+  return (
+    <div>
+      <main>
+        <section className="sec-portada">
+          <img
+            className="sec-portada-fondo"
+            src={fondoSpiderMan}
+            style={{ transform: `translateY(${movimiento * -0.12}px)` }}
+            alt=""
+          />
+          <img
+            className="sec-portada-personaje"
+            src={spiderManPng}
+            alt=""
+            style={{ transform: `translateY(${movimiento * 0.2}px)` }}
+          />
+          <h1 className="sec-portada-title animate__animated animate__zoomIn ">
+            Spider man
+          </h1>
+        </section>
+
+        <section className="sec-info">
+          <div ref={secInfoConteinerText} className={`sec-info-conteiner-text ${showAnimation ? "isActiveAnimation" :""} `}>
+            <h2 className="sec-info-title">Mais Morales</h2>
+            <span className="sec-info-descripcion">
+              Mais morales es un chico latinoamericano que le gusta la fisica
+              cuantica pero como como a cual adolecente hace las cosas que no
+              <br /> le gusta por la culpa de sus padre,pero todo iba cambiar un
+              dia...
+            </span>
+          </div>
+          <img
+            className="sec-info-img"
+            style={{ transform: `translateY(${movimiento * 0.1}px)` }}
+            src={araña}
+            alt=""
+          />
+        </section>
+
+        <section className="sec-img">
+          <div className="conteiner">
+            <div className="conteiner-img">
+              {history.map((img) => {
+                return (
+                  <div key={img.id} className="img">
+                    <img
+                      onClick={() => setObtenerID(img.id)}
+                      className="img-spider-man"
+                      src={img.img}
+                    />
+                  </div>
+                );
+              })}
+              
+            </div>
+            <div className="conteiner-show">
+              <div className="show">
+                {history.map((img) => {
+                  if (img.id === obtenerID) {
+                    return (
+                      <img key={img.id} className="show-imgID" src={img.img} />
+                    );
+                  }
+                })}
+              </div>
+              <div className="info">
+                <span className="descripcion-imgID">
+                  <span className="descripcion-imgID">{wordObtenidas}</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="contacto">
+          <h3 className="contacto-text">Contacto</h3>
+          <a
+            className="contacto-instagram"
+            href="https://www.instagram.com/yoel.gds/"
+          >
+            Instagram
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
---
+++ src/info.jsx
@@ -0,0 +1,26 @@
+import img1 from "./img/spider-man (5).png"
+import img2 from "./img/spider-man (3).png"
+import img3 from "./img/spider-man (1).png"
+import img4 from "./img/spider-man (4).png"
+export const history = [
+  {
+    id: 1,
+    img: img2,
+    descripcion: 'AMiles Morales es un adolescente común de Brooklyn que, tras ser mordido por una araña radiactiva, descubre que ha adquirido poderes similares a los de Spider-Man. Al principio, lucha para controlar sus habilidades y su nueva identidad de superhéroe.'
+  },
+  {
+    id: 2,
+    img: img4,
+    descripcion: 'AEn medio del caos, Miles se encuentra con Peter Parker, el Spider-Man original de su dimensión, pero se da cuenta de que él no es el único Spider-Man en el multiverso. Juntos, descubren la existencia de múltiples versiones de Spider-Man provenientes de diferentes realidades paralelas.'
+  },
+  {
+    id: 3,
+    img: img3,
+    descripcion: 'AUna amenaza interdimensional surge cuando el Kingpin, un poderoso villano, crea una máquina que puede abrir portales a otras dimensiones. Esto provoca que Spider-Men y Spider-Women de distintas realidades se unan para detener al Kingpin y salvar el multiverso.'
+  },
+  {
+    id: 4,
+    img: img1,
+    descripcion: 'AA medida que la batalla se intensifica, Miles debe enfrentar sus propios miedos e inseguridades para convertirse en el héroe que su ciudad necesita. Con el apoyo y la sabiduría de sus compañeros Spider-Man, Miles abraza su destino y asume el manto del héroe arácnido en su propia dimensión.'
+  },
+];
